40d84b4065f79fca91e464eec8bf6707d7629493
DreadLord <93027651+DreadLord025@users.noreply.github.com>
2023-12-26 00:51:54 +0200

    Initial commit

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using OrderHome.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Order.Controllers
+{
+    public class OrderController : Controller
+    {
+        [HttpPost]
+        public IActionResult Register(User request)
+        {
+            if (request.Age >= 16) return View("Quantity");
+            else return View("UnderAge");
+        }
+
+        [HttpPost]
+        public IActionResult ShowProducts(int quantity)
+        {
+            List<Product> products = new List<Product>();
+            for (int i = 0; i < quantity; i++) products.Add(new Product { Name = "Тип пиццы " + (i + 1).ToString(), Quantity = 0 });
+            return View("Products", products);
+        }
+
+        [HttpPost]
+        public IActionResult OrderProducts(List<Product> products)
+        {
+            var filteredProducts = from product in products where product.Quantity != 0 select product;
+            return View("Order", filteredProducts.ToList<Product>());
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OrderHome.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestID { get; set; }
+        public bool ShowRequestID => !string.IsNullOrEmpty(RequestID);
+    }
+}
